c2dc29fd501d27c7a2c19016f01f011a9b6bd1a8
ankit <ankitpanchal41106@gmail.com>
2026-06-12 11:41:48 +0530

    commenting in code done

--- CacheUtility/getSet.go
+++ CacheUtility/getSet.go
@@ -8,7 +8,11 @@ import (
 	"strings"
 )
 
+// preRequiste is to make a CSV file to hold the data of map after the program got terminated
+
 func cacheUtil(mpp *map[KEY]VALUE) {
+
+	// open the csv file in append and readWrite mode
 	db, err := os.OpenFile("D:\\BTECH\\INTERN_BUSY\\Projects\\CacheUtility\\data.csv", os.O_APPEND|os.O_RDWR, 0644)
 
 	if err != nil {
@@ -16,21 +20,26 @@ func cacheUtil(mpp *map[KEY]VALUE) {
 		return
 	}
 
-	defer db.Close()
+	defer db.Close() // close the file
 
-	dbInfo, _ := db.Stat()
+	dbInfo, _ := db.Stat() // collect info about the file (Size particularly)
 
-	Scanner := bufio.NewScanner(os.Stdin)
+	Scanner := bufio.NewScanner(os.Stdin) // decalre a Scanner to scan Inputs from terminal
 
+	// checking File size
 	if dbInfo.Size() != 0 {
+		// filesize is non zero
+		
+		// create a CSV reader on file
 		csvReader := csv.NewReader(db)
 		fmt.Println("\nHIT 'P' to enter persistent mode or 'C' to clear cache")
 
+		// using the scanner for scanninf input either P or C
 		Scanner.Scan()
 
 		ch := strings.TrimSpace(Scanner.Text())
 
-		switch ch {
+		switch ch { // switch on the basis of input if P then load file data in MPP otherwise Truncate file on C
 		case "P":
 			fmt.Println("Retriving Prior data")
 			dbData, _ := csvReader.ReadAll()
@@ -40,15 +49,17 @@ func cacheUtil(mpp *map[KEY]VALUE) {
 			}
 
 		case "C":
-			db.Close()
+			db.Close() // closing the previous file handle as OS prevents File Truncation in APPEND mode
 
+			// reOpen file in W only and truncate mode
 			cleanDb, err := os.OpenFile("D:\\BTECH\\INTERN_BUSY\\Projects\\CacheUtility\\data.csv", os.O_WRONLY|os.O_TRUNC, 0644)
 			if err != nil {
 				fmt.Println("Error in opening file for cleaning", err)
 				return
 			}
+			defer cleanDb.Close()
 
-			err2 := cleanDb.Truncate(0)
+			err2 := cleanDb.Truncate(0) // clear data
 
 			if err2 != nil {
 				fmt.Println("Error while clearing the cache", err)
@@ -60,6 +71,8 @@ func cacheUtil(mpp *map[KEY]VALUE) {
 		}
 
 	}
+
+	// reopening file here Because if user hit C then the file is closed already
 	dbReopen, err := os.OpenFile("D:\\BTECH\\INTERN_BUSY\\Projects\\CacheUtility\\data.csv", os.O_APPEND|os.O_RDWR, 0644)
 
 	if err != nil {
@@ -73,7 +86,8 @@ func cacheUtil(mpp *map[KEY]VALUE) {
 
 	for {
 		fmt.Print("\n->")
-
+		 
+		// scan for input , if null then break loop
 		if !Scanner.Scan() {
 			break
 		}
@@ -82,14 +96,13 @@ func cacheUtil(mpp *map[KEY]VALUE) {
 			continue
 		}
 
-		args := strings.Fields(Scanner.Text())
-
+		args := strings.Fields(Scanner.Text()) // extract args from the input
 		if len(args) == 0 {
 			fmt.Println("Enter something then hit enter")
 			continue
 		}
 
-		switch args[0] {
+		switch args[0] { // switch on the baais of args
 		case "SET":
 			if len(args) != 3 {
 				fmt.Println("format to set value is SET <KEY> <VALUE>")
@@ -97,7 +110,7 @@ func cacheUtil(mpp *map[KEY]VALUE) {
 			}
 			(*mpp)[args[1]] = args[2]
 
-			recordWriter := csv.NewWriter(dbReopen)
+			recordWriter := csv.NewWriter(dbReopen) // create a writer to write new record in File
 			newRow := []string{args[1], args[2]}
 			if err := recordWriter.Write(newRow); err != nil {
 				fmt.Println("error while writing")
@@ -107,8 +120,8 @@ func cacheUtil(mpp *map[KEY]VALUE) {
 			fmt.Println("Value Setted")
 
 		case "GET":
-			val,ok:=(*mpp)[args[1]]
-			if !ok{
+			val, ok := (*mpp)[args[1]]
+			if !ok {
 				fmt.Println("Key dont exists :( ")
 				continue
 			}
--- CacheUtility/main.go
+++ CacheUtility/main.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 )
 
+// creating 2 empty interfaces to Add flexiblity in key value type of data
 type KEY any
 type VALUE any
 
 func main() {
 
 	fmt.Println("Cache util")
-	DataStorage := make(map[KEY]VALUE)
-	cacheUtil(&DataStorage)
+	
+	DataStorage := make(map[KEY]VALUE) // declare a Map to store key value pairs when program is running
+
+	cacheUtil(&DataStorage) // calling cache utility
+	
 }
